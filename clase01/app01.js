c68034f9c55e7626dce648a2bced5b98c5d46ebd
liquid <hypergalactus@gmail.com>
2022-08-20 00:11:09 -0700

    first commit

---
+++ clase01/app01.js
@@ -0,0 +1,27 @@
+let uno = 1
+let dos = 2
+let saludo = 'adios popo'
+const pepo = 'Mickey los zetas estan afuera'
+
+console.log('Hola mundo', saludo)
+console.log(uno)
+console.log(pepo)
+
+uno = "hola"
+
+console.log(uno)
+
+//Tipos de datos
+//string
+//number
+//boolean
+
+//Manejo de if
+if(2 ==! false){
+    console.log('if')
+} else if(2 ==! 3){
+    console.log('else if')
+} else{
+    console.log('else')
+}
+
