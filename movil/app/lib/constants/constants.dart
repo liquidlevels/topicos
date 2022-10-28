2f836caaccf59655205ffd4d52506cf93c61fc7b
liquid <hypergalactus@gmail.com>
2022-10-27 21:35:31 -0700

    Despliega listas en diferente contenedor

---
+++ movil/app/lib/constants/constants.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+//Text
+
+const kAppTitle = 'uwu';
+const kHeaderTitle = 'awita de uwu';
+
+//Colors
+
+const kPrimaryColor = Color(0xff0a0908);
+const kSecondaryColor = Colors.deepPurple;
+const kTextColor = Color(0xffffffff);
+
+//Styles
+
+const kTextStyleHeaderTitle =
+    TextStyle(color: kTextColor, fontSize: 40, fontWeight: FontWeight.bold);
+
+const kTextStyleFruitsText = TextStyle(color: kTextColor, fontSize: 16);
--- movil/app/lib/screens/home.dart
+++ movil/app/lib/screens/home.dart
@@ -1,44 +1,59 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:app/constants/constants.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
+    const List<String> frutas = [
+      'Manzana',
+      'Platano',
+      'Aguacate',
+      'Uva',
+      'Naranja',
+      'Melon'
+    ];
+
+    const List<IconData> iconos = [
+      Icons.abc,
+      Icons.nat,
+      Icons.mail,
+      Icons.zoom_in_map,
+      Icons.games,
+      Icons.label
+    ];
+
+    const List<AssetImage> testIcons = [
+      AssetImage('assets/icons/bananas.png'),
+      AssetImage('assets/icons/grapes.png'),
+      AssetImage('assets/icons/lemon.png'),
+      AssetImage('assets/icons/orange.png'),
+      AssetImage('assets/icons/strawberry.png'),
+      AssetImage('assets/icons/watermelon.png')
+    ];
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('About me'),
         backgroundColor: Colors.black,
-        leading: const Icon(
-          Icons.fireplace,
-          color: Colors.white,
-          size: 40,
-        ),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Container(
-            color: Colors.red,
-            width: 100,
-            height: 100,
-            child: const Text('TURIP'),
-          ),
-          Container(
-              padding: const EdgeInsets.only(top: 30),
-              color: Colors.black,
-              width: double.infinity,
-              height: 100,
-              child: const Text(
-                'Copyright @2022',
-                style: TextStyle(color: Colors.white),
-                textAlign: TextAlign.center,
-              )),
-        ],
-      ),
-    );
+        body: SafeArea(
+          child: ListView.builder(
+              itemCount: frutas.length,
+              itemBuilder: ((context, index) {
+                return Container(
+                    margin: const EdgeInsets.all(10),
+                    padding: const EdgeInsets.all(10),
+                    color: kSecondaryColor,
+                    width: double.infinity,
+                    height: 50,
+                    child: Row(children: [
+                      Icon(iconos[index]),
+                      Text(
+                        frutas[index],
+                        style: kTextStyleFruitsText,
+                        textAlign: TextAlign.center,
+                      )
+                    ]));
+              })),
+        ));
   }
 }
