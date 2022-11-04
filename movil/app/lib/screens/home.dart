bcd0d460292a2242135603d255929f61e066f922
liquid <hypergalactus@gmail.com>
2022-11-03 18:06:01 -0700

    Clase 03/11/22 - Card & extract method

--- movil/app/lib/screens/home.dart
+++ movil/app/lib/screens/home.dart
@@ -1,3 +1,4 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:app/constants/constants.dart';
 
@@ -6,54 +7,68 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const List<String> frutas = [
-      'Manzana',
-      'Platano',
-      'Aguacate',
-      'Uva',
-      'Naranja',
-      'Melon'
-    ];
-
-    const List<IconData> iconos = [
-      Icons.abc,
-      Icons.nat,
-      Icons.mail,
-      Icons.zoom_in_map,
-      Icons.games,
-      Icons.label
-    ];
-
-    const List<AssetImage> testIcons = [
-      AssetImage('assets/icons/bananas.png'),
-      AssetImage('assets/icons/grapes.png'),
-      AssetImage('assets/icons/lemon.png'),
-      AssetImage('assets/icons/orange.png'),
-      AssetImage('assets/icons/strawberry.png'),
-      AssetImage('assets/icons/watermelon.png')
-    ];
-
     return Scaffold(
-        backgroundColor: Colors.black,
+        backgroundColor: Colors.amber,
         body: SafeArea(
-          child: ListView.builder(
-              itemCount: frutas.length,
-              itemBuilder: ((context, index) {
-                return Container(
-                    margin: const EdgeInsets.all(10),
-                    padding: const EdgeInsets.all(10),
-                    color: kSecondaryColor,
-                    width: double.infinity,
-                    height: 50,
-                    child: Row(children: [
-                      Icon(iconos[index]),
-                      Text(
-                        frutas[index],
-                        style: kTextStyleFruitsText,
-                        textAlign: TextAlign.center,
-                      )
-                    ]));
-              })),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const CircleAvatar(
+                  radius: 100,
+                  backgroundImage: AssetImage('assets/images/malacardo.png'),
+                ),
+                const Text('@liquidlevels',
+                    style: TextStyle(
+                        fontSize: 30,
+                        color: Colors.black,
+                        fontFamily: 'Poor Story')),
+                const Text('Minecraft player since 2013',
+                    style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.black,
+                        fontFamily: 'Monocraft')),
+                const Card(
+                    color: Colors.black,
+                    child: ListTile(
+                      leading: Icon(Icons.mail, color: Colors.amber),
+                      title: Text(
+                        'liquid@me.com',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      subtitle: Text(
+                        'mail',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      trailing: Icon(
+                        Icons.arrow_forward_ios,
+                        color: Colors.amber,
+                      ),
+                    )),
+                cartaPersonalizada('liquidlevels was here', Icons.abc, 'uwu'),
+                cartaPersonalizada(
+                    'ding dong', Icons.access_alarm, 'levantate prro')
+              ],
+            ),
+          ),
+        ));
+  }
+
+  Card cartaPersonalizada(String data, IconData icon, String subtitle) {
+    return Card(
+        // margin: EdgeInsets.only(top: 10),
+        // padding: const EdgeInsets.all(6),
+        // height: 50,
+        // width: 300,
+        color: Colors.black,
+        child: ListTile(
+          leading: Icon(icon, color: Colors.amber),
+          title: Text(data, style: const TextStyle(color: Colors.white)),
+          subtitle: Text(subtitle, style: const TextStyle(color: Colors.white)),
+          trailing: const Icon(
+            Icons.arrow_forward_ios,
+            color: Colors.amber,
+          ),
         ));
   }
 }
